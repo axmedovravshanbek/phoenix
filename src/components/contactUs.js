9acd69c52567655b740d31c01e2a6e469ec07f06
airfun <ahmedovravshanbek21@gmail.com>
2022-03-18 03:17:05 +0500

    send padding fix

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -5,7 +5,7 @@ import {observer} from "mobx-react-lite";
 
 const ContactUs = () => {
     return (
-        <div className='container z2 mt-120 mb-120'>
+        <div className='container z2 mb-120'>
             <h2 className='pb-2 mb-4'>{lang.contactWithUs[xStore.lang]}</h2>
             <div style={{backgroundColor: '#F5F5F7', overflow:'hidden'}}>
                 <div className="row">
@@ -22,14 +22,14 @@ const ContactUs = () => {
                                            className='mb-4 w-100'/>
                                 </div>
                                 <div className="col-12">
-                                    <textarea type="text" className='col-12 w-100'
+                                    <textarea className='col-12 w-100'
                                            placeholder={lang.messagePlaceholder[xStore.lang]}/>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-md-5 d-flex justify-content-lg-end justify-content-center">
-                        <div className='d-flex align-items-center px-4'>
+                        <div className='d-flex align-items-center p-4'>
                             <div className="circle220 send-button orange-hover">
                                 <div className='wave'/>
                                 <div className='wave'/>
